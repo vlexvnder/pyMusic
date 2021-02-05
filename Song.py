27054ac38e992e98397dd46d4cc3e7a051d351e8
Alexander Goldberg <38928216+vlexvnder@users.noreply.github.com>
2021-02-04 22:33:51 -0800

    Create Song.py

---
+++ Song.py
@@ -0,0 +1,37 @@
+import Song from Piano
+
+s = Song()
+s.setTempo(60)
+
+#Put song here
+s.addNote(Note(1,"G4",1))
+s.addNote(Note(2,"G4",1))
+s.addNote(Note(3, "D4",1))
+s.addNote(Note(4, "D4",1))
+s.addNote(Note(5, "E4",1))
+s.addNote(Note(6, "E4",1))
+s.addNote(Note(7, "D4",1))
+s.addNote(Note(8, "C4",1))
+s.addNote(Note(9, "C4",1))
+s.addNote(Note(10, "B4",1))
+s.addNote(Note(11, "B4",1))
+s.addNote(Note(12, "A4",1))
+s.addNote(Note(13, "A4",1))
+s.addNote(Note(14, "G4",1))
+s.addNote(Note(15, "D4",1))
+s.addNote(Note(16, "S", 3))
+s.addNote(Note(17, "D4",1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+(s.compileSong()).export("s1.mp3", format="mp3")
